2ab7f9725c0a6b4c0912dd6d40432535fd672bab
Yosef Lehrer <yolehrer@gmail.com>
2019-05-31 14:27:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,10 +16,10 @@ def speak_to_grandma("phrase")
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
-  if phrase == phrase.upcase
+  if phrase != phrase.upcase
   return "NO, NOT SINCE 1938!"
 end
-  if phrase != phrase.upcase
+  if phrase = phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
